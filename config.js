092a531cf7d34d21253b660330f53100a03f6659
Ami0408 <155718814+Ami0408@users.noreply.github.com>
2024-01-05 11:50:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
+global.owner =
+  ['09131388324', 'Amiola's bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
